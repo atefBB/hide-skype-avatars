0d761e63161bbe7da6ff7e4760808498e75335ff
Atef Ben Ali <atef.bettaib@gmail.com>
2018-04-18 07:56:52 +0200

    create the chrome extension

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var avatars = document.getElementsByTagName('swx-avatar');
-var avatarsImgs = document.getElementsByClassName('Avatar-image');
-
-for(var avatar in avatars) {
-   if(!isNaN(avatar)) {
-     avatars[avatar].setAttribute('style', 'display: none;');
-   }
-}
-
-for(var avatarImg in avatarsImgs) {
-   if(!isNaN(avatarImg)) {
-    avatarsImgs[avatarImg].setAttribute('style', 'display: none;')
-   }
-}
+/**
+ * @author Atef Ben Ali
+ */
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function() {
+	// No tabs or host permissions needed!
+  chrome.tabs.executeScript({
+    code: 'var avatars = document.getElementsByTagName("swx-avatar");'
+          + 'var avatarsImgs = document.getElementsByClassName("Avatar-image");'
+          + 'for(var avatar of avatars) {'
+            + 'avatar.setAttribute("style", "display: none;");'
+          + '}'
+          + 'for(var avatarImg of avatarsImgs) {'
+          +   'avatarImg.setAttribute("style", "display: none;");'
+          + '}'
+  });
+});
